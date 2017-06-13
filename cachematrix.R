7e16e23725a30c0e91fae0cd5e68e3e2f4b8b9dc
Xin Chen <chenx26@uw.edu>
2017-06-13 09:09:14 -0600

    code for cache matrix and some test code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function to create cached Matrix that stores the original matrix
+## and the inverse of the original matrix
+## with get and set functions for both of them
 
-## Write a short comment describing this function
+## create cached Matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  m = NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## a function with cached Matrix as input
+## returns the inverse of the Matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  m <- x$getInverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
+
+## some test code
+
+A = matrix(c(1,2,3,4), nrow = 2)
+A_inv = solve(A)
+res = makeCacheMatrix()
+res$set(A)
+res$get()
+res$getInverse()
+# no cached data, compute inverse
+cacheSolve(res)
+
+# use cached data instead of computing the inverse
+cacheSolve(res)
